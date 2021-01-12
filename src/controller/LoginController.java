2b78cadb13a23b7fcef51ab382bc515067a38f5e
trungbe <trungnd0601@gmail.com>
2021-01-12 10:26:14 +0700

    create login form

---
+++ src/controller/LoginController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+}
+
